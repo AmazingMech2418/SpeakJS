8ddaeaddd7e764aee199cb25ede410c3cbd90982
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-06-18 15:59:22 -0400

    Update recognition.js

--- recognition.js
+++ recognition.js
@@ -4,3 +4,9 @@ speechdata.lang = lang;
 speechdata.onresult = callback;
 speechdata.start();
 }
+// This is not needed, but can be used as a callback to get the recognized speech into a variable.
+// Just use recognize(speechToVar,"en"); to get the recognized speech in English.
+var speechRecognized;
+function speechToVar(e) {
+speechRecognized = e.results;
+}
